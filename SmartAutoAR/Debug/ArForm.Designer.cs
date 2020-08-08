9c1a544a7ecaa5ee7338e7a493309e23b3b8ae3a
Max Zheng <34936931+54bp6cl6@users.noreply.github.com>
2020-08-08 09:00:16 +0800

    補上傳

--- SmartAutoAR/Debug/ArForm.Designer.cs
+++ SmartAutoAR/Debug/ArForm.Designer.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace Debug
+﻿namespace Debug
 {
 	partial class ArForm
 	{
@@ -30,7 +28,6 @@ namespace Debug
 		/// </summary>
 		private void InitializeComponent()
 		{
-			
 			this.components = new System.ComponentModel.Container();
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
