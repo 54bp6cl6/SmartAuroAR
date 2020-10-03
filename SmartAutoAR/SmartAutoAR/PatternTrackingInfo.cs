c2f5dc03bd41ff6d57816f548938a69066db11b7
Max Zheng <54bp6cl6@gmail.com>
2020-10-03 12:06:21 +0800

    完成 marker 偵測
    marker偵測大功告成 太爽啦

---
+++ SmartAutoAR/SmartAutoAR/PatternTrackingInfo.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using OpenCvSharp;
+using OpenCvSharp.Aruco;
+using OpenTK;
+
+namespace OpenCVMarkerLessAR
+{
+	/// <summary>
+	/// Pattern tracking info.
+	/// This code is a rewrite of https://github.com/MasteringOpenCV/code/tree/master/Chapter3_MarkerlessAR using "OpenCV for Unity".
+	/// </summary>
+	public class PatternTrackingInfo
+	{
+		public Mat homography;
+		public Mat points2d;
+		public Point2f[] pts;
+		public Matrix4 pose3d;
+		public Mat campos;
+		public Point2d center;
+		public Mat imgPts;
+		public Mat raux, taux;
+		public Matrix4 ViewMatrix { get; protected set; }
+		public Vector3 CameraPosition { get; protected set; }
+
+		protected float[,] cameraMatrix = new float[3, 3]
+		{
+			{ 1.72933044e+03f, 0.00000000e+00f, 6.38250933e+02f },
+		    { 0.00000000e+00f, 1.52595386e+03f, 3.21003959e+02f },
+			{ 0, 0, 1 }
+		};
+
+		protected float[] dist = new float[]
+		{ 4.83589141e-01f ,- 5.85278583e+00f ,- 1.42979696e-02f ,- 1.31067357e-02f,  1.86111848e+01f };
+
+		public PatternTrackingInfo()
+		{
+			homography = new Mat();
+			points2d = new Mat();
+			campos = new Mat();
+		}
+
+		public void ComputePose()
+		{
+			raux = new Mat();
+			taux = new Mat();
+
+			List<Vec3d> rvecs = new List<Vec3d>(), tvecs = new List<Vec3d>();
+			CvAruco.EstimatePoseSingleMarkers(
+				new Point2f[][] { pts },
+				1,
+				InputArray.Create(cameraMatrix),
+				InputArray.Create(dist),
+				OutputArray.Create<Vec3d>(rvecs),
+				OutputArray.Create<Vec3d>(tvecs));
+			ViewMatrix = GetViewMatrix(rvecs[0], tvecs[0]);
+		}
+
+		private Matrix4 GetViewMatrix(Vec3d rvec, Vec3d tvec)
+		{
+			Mat rotMat = new Mat();
+			Cv2.Rodrigues(rvec, rotMat);
+
+			Matrix4 output = new Matrix4(
+				(float)rotMat.At<double>(0, 0), (float)rotMat.At<double>(0, 2), -(float)rotMat.At<double>(0, 1), (float)tvec[0],
+				-(float)rotMat.At<double>(1, 0), -(float)rotMat.At<double>(1, 2), (float)rotMat.At<double>(1, 1), -(float)tvec[1],
+				-(float)rotMat.At<double>(2, 0), -(float)rotMat.At<double>(2, 2), (float)rotMat.At<double>(2, 1), -(float)tvec[2],
+				0, 0, 0, 1);
+
+			output.Transpose();
+
+			rotMat.Dispose();
+
+			return output;
+		}
+
+		public Matrix4 GetProjectionMatrix(float width, float height, float near_plane = 0.0001f, float far_plane = 10000f)
+		{
+			float fx = cameraMatrix[0, 0];
+			float fy = cameraMatrix[1, 1];
+			float cx = cameraMatrix[0, 2];
+			float cy = cameraMatrix[1, 2];
+
+			Matrix4 pm = new Matrix4()
+			{
+				Row0 = new Vector4(2 * fx / width, 0, 0, 0),
+				Row1 = new Vector4(0, 2 * fy / height, 0, 0),
+				Row2 = new Vector4(1 - 2 * cx / width, 2 * cy / height - 1, -(far_plane + near_plane) / (far_plane - near_plane), -1),
+				Row3 = new Vector4(0, 0, -(2 * far_plane * near_plane) / (far_plane - near_plane), 0)
+			};
+			return pm;
+		}
+	}
+}
