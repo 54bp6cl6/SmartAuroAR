bfea4cd695c93380ef220fa8561d2ceafa0cb0ad
Max Zheng <54bp6cl6@gmail.com>
2020-10-17 11:48:58 +0800

    整理程式

--- SmartAutoAR/SmartAutoAR/InputSource/IInputSource.cs
+++ SmartAutoAR/SmartAutoAR/InputSource/IInputSource.cs
@@ -7,9 +7,24 @@ namespace SmartAutoAR.InputSource
 	/// </summary>
 	public interface IInputSource
 	{
+		/// <summary>
+		/// 呼叫影像來源回傳欲取得的影像
+		/// </summary>
 		public Bitmap GetInputFrame();
+
+		/// <summary>
+		/// 回傳影像的寬度
+		/// </summary>
 		public int OutputWidth { get; }
+
+		/// <summary>
+		/// 回傳影像的高度
+		/// </summary>
 		public int OutputHeight { get; }
+
+		/// <summary>
+		/// 回傳影像的長寬比
+		/// </summary>
 		public float AspectRatio { get; }
 	}
 }
