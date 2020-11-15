abbfa8b51b8ba193ff4c79661cd93098194707cd
Max Zheng <54bp6cl6@gmail.com>
2020-11-15 09:33:21 +0800

    IDK

--- SmartAutoAR/LearnSmartAutoAR/ArForm.cs
+++ SmartAutoAR/LearnSmartAutoAR/ArForm.cs
@@ -9,88 +9,88 @@ using Bitmap = System.Drawing.Bitmap;
 
 namespace LearnSmartAutoAR
 {
-    public partial class ArForm : GameWindow
-    {
-        VideoSource videoSource;
-        Scene scene;
-        Model stoneMan;
-        ArWorkflow workflow;
-        const string ResourcesPath = @"..\..\..\..\resources\";
+	public partial class ArForm : GameWindow
+	{
+		VideoSource videoSource;
+		Scene scene;
+		Model stoneMan;
+		ArWorkflow workflow;
+		const string ResourcesPath = @"..\..\..\..\resources\";
 
-        public ArForm(int width, int height, string title) :
-            base(width, height,
-                GraphicsMode.Default,
-                title,
-                GameWindowFlags.Default,
-                DisplayDevice.Default,
-                4, 5,
-                GraphicsContextFlags.ForwardCompatible)
-        { }
+		public ArForm(int width, int height, string title) :
+			base(width, height,
+				GraphicsMode.Default,
+				title,
+				GameWindowFlags.Default,
+				DisplayDevice.Default,
+				4, 5,
+				GraphicsContextFlags.ForwardCompatible)
+		{ }
 
-        protected override void OnLoad(EventArgs e)
-        {
-            // 請輸入您的影片路徑
-            videoSource = new VideoSource(ResourcesPath+"video_test.mp4");
+		protected override void OnLoad(EventArgs e)
+		{
+			// 請輸入您的影片路徑
+			videoSource = new VideoSource(ResourcesPath + "video_test.mp4");
 
-            // 創建場景
-            scene = new Scene();
-            stoneMan = Model.LoadModel(ResourcesPath+@"models\Stone\Stone.obj"); // 請輸入您的模型路徑
-            scene.Models.Add(stoneMan);
+			// 創建場景
+			scene = new Scene();
+			stoneMan = Model.LoadModel(ResourcesPath + @"models\Stone\Stone.obj"); // 請輸入您的模型路徑
+			scene.Models.Add(stoneMan);
 
-            // 調整模型大小
-            stoneMan.Resize(0.5f);
+			// 調整模型大小
+			stoneMan.Resize(0.5f);
 
-            // 加入燈光
-            // scene.Lights.Add(new AmbientLight(Color4.White, 0.8f));
+			// 加入燈光
+			// scene.Lights.Add(new AmbientLight(Color4.White, 0.8f));
 
-            // 建立 workflow 物件
-            workflow = new ArWorkflow(videoSource);
+			// 建立 workflow 物件
+			workflow = new ArWorkflow(videoSource);
 
-            // 設定 marker 對應的 scene
-            Bitmap marker = new Bitmap(ResourcesPath+"marker.png"); // 請輸入您 Marker 圖檔的路徑
-            workflow.MarkerPairs[marker] = scene;
-            workflow.TrainMarkers(); // 修改後一定要執行!!
+			// 設定 marker 對應的 scene
+			Bitmap marker = new Bitmap(ResourcesPath + "marker.png"); // 請輸入您 Marker 圖檔的路徑
+			workflow.MarkerPairs[marker] = scene;
+			workflow.TrainMarkers(); // 修改後一定要執行此函數!!
 
-            // 開啟光源追蹤模組
-            workflow.EnableLightTracking = true;
+			// 開啟光源追蹤模組
+			workflow.EnableLightTracking = true;
 
-            // 開啟色彩融合模組
-            workflow.EnableColorHarmonizing = true;
+			// 開啟色彩融合模組
+			workflow.EnableColorHarmonizing = true;
 
-            base.OnLoad(e);
-        }
+			base.OnLoad(e);
+		}
 
-        protected override void OnRenderFrame(FrameEventArgs e)
-        {
-            // 確保視窗比例與背景一致
-            Width = (int)(Height * workflow.WindowAspectRatio);
+		protected override void OnRenderFrame(FrameEventArgs e)
+		{
+			// 確保視窗比例與背景一致
+			Width = (int)(Height * workflow.WindowAspectRatio);
 
-            // 當影片撥放完畢後重播
-            if (videoSource.EndOfVideo)
-            {
-                videoSource.Replay();
-                workflow.ClearState();
-            }
+			// 當影片撥放完畢後重播
+			if (videoSource.EndOfVideo)
+			{
+				videoSource.Replay();
+				workflow.ClearState();
+			}
 
-            workflow.Show();
-            SwapBuffers(); // 這是 GameWindow 中繪製新畫面必須的語法
+			workflow.Show();
+			SwapBuffers(); // 這是 GameWindow 中繪製新畫面必須的語法
 
-            base.OnRenderFrame(e);
-        }
+			base.OnRenderFrame(e);
+		}
 
-        protected override void OnResize(EventArgs e)
-        {
-            // 設定影像輸出大小
-            workflow.SetOutputSize(Width, Height);
+		protected override void OnResize(EventArgs e)
+		{
+			// 設定影像輸出大小
+			workflow.SetOutputSize(Width, Height);
 
-            base.OnResize(e);
-        }
+			base.OnResize(e);
+		}
 
-        protected override void OnUnload(EventArgs e)
-        {
-            scene.Dispose();
+		protected override void OnUnload(EventArgs e)
+		{
+			scene.Dispose();
 
-            base.OnUnload(e);
-        }
-    }
+			base.OnUnload(e);
+		}
+	}
 }
